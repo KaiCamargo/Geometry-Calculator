c697e702151d95eaed9c30100b4d539ab8a8c1ce
Kai Camargo <kaicamargo@protonmail.com>
2026-09-23 16:48:10 -0700

    Update GeometryCalculator.cpp
    Added version control and created main function

---
+++ GeometryCalculator.cpp
@@ -0,0 +1,14 @@
+/*
+	Program Name: GeometryCalculator.cpp
+	Programmer: Kai Tanaka Camargo
+	Date: 9-23-2026
+	Version: 1.0
+	A program using switch and case that calculates 
+	the area of a circle, rectangle, or triangle
+	depending on the user input, using a sentinel
+	value to exit the program.
+*/
+
+int main() {
+	return 0;
+} // End of program
